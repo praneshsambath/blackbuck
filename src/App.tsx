3385bc06c25d5c9e8dda3db4ccc7ed4475508b4a
praneshsambath <praneshsambath@eynos.com>
2018-08-20 19:45:17 +0530

    newScreen

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { DatePicker } from 'antd';
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <DatePicker />
+    );
+  }
+}
+
+export default App;
